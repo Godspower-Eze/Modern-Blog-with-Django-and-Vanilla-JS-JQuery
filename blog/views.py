2ac21489d6dddc164027a5b9a9224a70ec197baa
Godspower <godspowereze260@gmail.com>
2020-10-06 10:08:51 +0100

    Added homepage

--- blog/views.py
+++ blog/views.py
@@ -4,8 +4,14 @@ from .models import Post
 
 def index(request):
     posts = Post.published.all()
+    first_two = posts[:2]
+    third = posts[3]
+    last_two = posts[3:]
     context = {
-        'posts': posts
+        'posts':posts,
+        'first_two': first_two,
+        'third': third,
+        'last_two': last_two
     }
     return render(request, 'blog/index.html', context)
 
