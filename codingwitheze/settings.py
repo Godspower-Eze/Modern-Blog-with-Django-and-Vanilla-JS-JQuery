bf4a2d5a20a8b776416acc498de2f040685c47bd
Godspower <godspowereze260@gmail.com>
2020-11-14 09:42:30 +0100

    passed the secret key, email user and email password to environmental varaibles

--- codingwitheze/settings.py
+++ codingwitheze/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#y2)o=#riihur8)g!okq4$-rjbm2u&@n84!!0fx87&^*n8==2='
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -135,6 +135,6 @@ CKEDITOR_CONFIGS = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'testingblog260@gmail.com'
-EMAIL_HOST_PASSWORD = 'testblog123'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
